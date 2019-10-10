7370bd582baf1cf9699b377d33d981fe2fa405ff
KishanJ29 <kishan29j@gmail.com>
2019-10-10 19:35:46 +0530

    edit

--- Master_i2c_tb.vhd
+++ Master_i2c_tb.vhd
@@ -6,11 +6,10 @@ entity Master_tb is
 end entity;
 
 architecture test of Master_tb is 
---	component Master_i2c is 
+	component Master_i2c is 
 --		port(	clk : in std_logic;
 --				rst :in std_logic;
 --				enable : in std_logic;
---				
 --				Data_Txn : in std_logic_vector(7 downto 0);
 --				Addr : in std_logic_vector(7 downto 0);
 --			--	RegAddrReq : in std_logic; --Request for Register Address 
@@ -19,9 +18,16 @@ architecture test of Master_tb is
 --				SCK : inout std_logic;
 --				RegSnd : out std_logic
 --			);
---	end component;
---
-	signal clk_in : std_logic:='0';
+	end component;
+	component DummyPiso is 
+	port(	sclk : in std_logic;
+			rst :in std_logic;
+			Data_Txn : in std_logic_vector(7 downto 0);
+			SDA : inout std_logic
+		);
+	end component;
+
+signal clk_in : std_logic:='0';
 	signal rst_in : std_logic;
 	signal enable_in : std_logic;
 	signal Data_Txn_in :  std_logic_vector(7 downto 0);
@@ -41,23 +47,32 @@ architecture test of Master_tb is
 	signal counter :  integer; 
 	signal address :  std_logic_vector(7 downto 0):="10101011";
 	signal reg :  std_logic_vector(7 downto 0):= "00000000";
-	alias FsmClk is << signal .master_tb.Master_DUT.FSM_Clk :std_logic>>;
-	alias LoadFlag is << signal .master_tb.Master_DUT.load :std_logic>>;
+--	alias FsmClk is << signal .master_tb.Master_DUT.FSM_Clk :std_logic>>;
+--	alias LoadFlag is << signal .master_tb.Master_DUT.load :std_logic>>;
+------------------------------------------------------------------------------------
+-- 			Slave
+---------------------------------------------------------------
+	signal sclk_slv, rst_slv, sda_slv : std_logic;
+	signal Data_Txn_slv : std_logic_vector(7 downto 0);
 	begin 
-	Master_DUT: entity work.Master_i2c port map(
-								 clk => clk_in,
-								 rst => rst_in,
-								 enable => enable_in,
-								 
-								 Data_Txn => Data_Txn_in,
-								 Addr => Addr_in ,
-								 Data_Rxn => Data_Rxn_in,
-								 SDA =>SDA_in,
-								 SCK => SCK_out,
-								 RegSnd => RegSnd_out
-								);	
-	
-	
+--	Master_DUT: entity work.Master_i2c port map(
+--								 clk => clk_in,
+--								 rst => rst_in,
+--								 enable => enable_in,
+--								 
+--								 Data_Txn => Data_Txn_in,
+--								 Addr => Addr_in ,
+--								 Data_Rxn => Data_Rxn_in,
+--								 SDA =>SDA_in,
+--								 SCK => SCK_out,
+--								 RegSnd => RegSnd_out
+--								);	
+	Slave_DUT: entity work.slave port map(
+										sclk => SCK_out,
+										rst => rst_in,
+										Data_Txn =>Data_Txn_slv,
+										SDA=> SDA_in
+										);
 	clk_Process: process(clk_in,finished)
 					begin
 					clk_in <= not clk_in after 10 ns when finished /= '1' else '0';
@@ -70,10 +85,10 @@ architecture test of Master_tb is
 					end if;
 					if(tb_count < 200)then
 						rst_in <= '1'; 
-						enable_in <= '0';
+						--enable_in <= '0';
 					else
 						rst_in <= '0';
-						enable_in <= '1';
+						--enable_in <= '1';
 					end if;
 					end process Clock_Counter;
  	
@@ -86,57 +101,151 @@ test_vector_generation: process(SCK_out)
 						Addr_in <= Addre_in;
 
 	 end procedure;
-		    -- read from I2C bus
---			    procedure i2c_read (
---			      constant address : in  std_logic_vector(6 downto 0);
---			      signal data      : out std_logic_vector(7 downto 0)) is
---			    begin
 
---			      i2c_set_read;
-
-
---			      i2c_read_ack(ack);
---			      if ack = '0' then
---			        state_dbg <= 6;
---			        i2c_stop;
---			        return;
---			      end if;
---			      ack       <= '0';
---			      state_dbg <= 4;
---			      i2c_receive_byte(data);
---			      state_dbg <= 5;
---			      i2c_write_nack;
---			      state_dbg <= 6;
---			      i2c_stop;
---			    end procedure i2c_read;
-
-	--Write
---    procedure i2c_write (
---		signal address : in std_logic_vector(7 downto 0);
---		signal reg :  std_logic_vector(7 downto 0):="00000000";
---		signal counter : out integer ) is
---		begin 
---			definitions (address,address);--( "10101011","11001101");
---			
---	end procedure;
 		 begin 
 					definitions ( address,"11001101");
-				if(rst_in  = '1')then
-					SDA_Trig <= '0';
-					counter <= 0;
-				elsif(rising_edge(SCK_out))then
-					if(counter = 8)then 
-						SDA_Trig <= '1';
-						SDA_slave <= '0';
-						counter <= 0;
-					else
-						SDA_Trig <= '1';
-						counter <= counter + 1;
-						reg <=  (SDA_slave & reg(7 downto 1));
-						assert(address = reg) report "Write Failed" severity warning;
-					end if;
-			 end if;
-	
+--				if(rst_in  = '1')then
+--					SDA_Trig <= '0';
+--					counter <= 0;
+--				elsif(rising_edge(SCK_out))then
+--					if(counter = 8)then 
+--						SDA_Trig <= '1';
+--						SDA_slave <= '0';
+--						counter <= 0;
+--					else
+--						SDA_Trig <= '1';
+--						counter <= counter + 1;
+--						reg <=  (SDA_slave & reg(7 downto 1));
+--						assert(address = reg) report "Write Failed" severity warning;
+--					end if;
+--			 end if;
+--	
 		end process  test_vector_generation;
-		SDA_in <= SDA_slave when counter = 8 else 'Z';
+		--SDA_in <= SDA_slave when counter = 8 else 'Z';
 	end test;
+-----------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+--							Previous Section
+------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+--	signal clk_in : std_logic:='0';
+--	signal rst_in : std_logic;
+--	signal enable_in : std_logic;
+--	signal Data_Txn_in :  std_logic_vector(7 downto 0);
+--	signal Addr_in :  std_logic_vector(7 downto 0);
+--	signal Data_Rxn_in: std_logic_vector(7 downto 0);
+--	signal SDA_in : std_logic;
+--	signal SDA_Trig : std_logic;
+--	signal SDA_slave :std_logic;
+--	signal SCK_out : std_logic;
+--	signal RegSnd_out : std_logic;
+--	signal tb_count : integer := 0;
+--	signal Slave_Driver : std_logic;
+--	signal wr_count	: integer := 0;
+--	signal finished : std_logic:= '0';
+--	signal start : std_logic := '0';
+--	--signal  ClockPeriod : time := 0 ns;
+--	signal counter :  integer; 
+--	signal address :  std_logic_vector(7 downto 0):="10101011";
+--	signal reg :  std_logic_vector(7 downto 0):= "00000000";
+----	alias FsmClk is << signal .master_tb.Master_DUT.FSM_Clk :std_logic>>;
+----	alias LoadFlag is << signal .master_tb.Master_DUT.load :std_logic>>;
+--	begin 
+--	Master_DUT: entity work.Master_i2c port map(
+--								 clk => clk_in,
+--								 rst => rst_in,
+--								 enable => enable_in,
+--								 
+--								 Data_Txn => Data_Txn_in,
+--								 Addr => Addr_in ,
+--								 Data_Rxn => Data_Rxn_in,
+--								 SDA =>SDA_in,
+--								 SCK => SCK_out,
+--								 RegSnd => RegSnd_out
+--								);	
+--	
+--	
+--	clk_Process: process(clk_in,finished)
+--					begin
+--					clk_in <= not clk_in after 10 ns when finished /= '1' else '0';
+--				end process clk_process;
+--
+--	Clock_Counter: process(Clk_in,tb_count)
+--					begin
+--					if rising_edge(Clk_in)then
+--						tb_count <= tb_count + 1;
+--					end if;
+--					if(tb_count < 200)then
+--						rst_in <= '1'; 
+--						enable_in <= '0';
+--					else
+--						rst_in <= '0';
+--						enable_in <= '1';
+--					end if;
+--					end process Clock_Counter;
+-- 	
+--test_vector_generation: process(SCK_out) 
+--	 procedure definitions (constant Data_in  :in  std_logic_vector(7 downto 0);
+--							constant Addre_in : in std_logic_vector(7 downto 0))is
+--					begin
+--						start <= '1';
+--						Data_Txn_in <= Data_in;
+--						Addr_in <= Addre_in;
+--
+--	 end procedure;
+--		    -- read from I2C bus
+----			    procedure i2c_read (
+----			      constant address : in  std_logic_vector(6 downto 0);
+----			      signal data      : out std_logic_vector(7 downto 0)) is
+----			    begin
+--
+----			      i2c_set_read;
+--
+--
+----			      i2c_read_ack(ack);
+----			      if ack = '0' then
+----			        state_dbg <= 6;
+----			        i2c_stop;
+----			        return;
+----			      end if;
+----			      ack       <= '0';
+----			      state_dbg <= 4;
+----			      i2c_receive_byte(data);
+----			      state_dbg <= 5;
+----			      i2c_write_nack;
+----			      state_dbg <= 6;
+----			      i2c_stop;
+----			    end procedure i2c_read;
+--
+--	--Write
+----    procedure i2c_write (
+----		signal address : in std_logic_vector(7 downto 0);
+----		signal reg :  std_logic_vector(7 downto 0):="00000000";
+----		signal counter : out integer ) is
+----		begin 
+----			definitions (address,address);--( "10101011","11001101");
+----			
+----	end procedure;
+--		 begin 
+--					definitions ( address,"11001101");
+--				if(rst_in  = '1')then
+--					SDA_Trig <= '0';
+--					counter <= 0;
+--				elsif(rising_edge(SCK_out))then
+--					if(counter = 8)then 
+--						SDA_Trig <= '1';
+--						SDA_slave <= '0';
+--						counter <= 0;
+--					else
+--						SDA_Trig <= '1';
+--						counter <= counter + 1;
+--						reg <=  (SDA_slave & reg(7 downto 1));
+--						assert(address = reg) report "Write Failed" severity warning;
+--					end if;
+--			 end if;
+--	
+--		end process  test_vector_generation;
+--		--SDA_in <= SDA_slave when counter = 8 else 'Z';
+--	end test;
+-----------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------
--- Slave.vhd
+++ Slave.vhd
@@ -2,42 +2,59 @@
 		use ieee.std_logic_1164.all;
 		use ieee.Numeric_std.all;
 
-entity DummyPiso is 
-	port(	clk : in std_logic;
+entity slave is 
+	port(	sclk : in std_logic;
 			rst :in std_logic;
-			
-			
 			Data_Txn : in std_logic_vector(7 downto 0);
 			SDA : inout std_logic
 		);
 end entity;
 
-architecture test of DummyPiso is
-	signal temp : std_logic_vector(7 downto 0);
+architecture test of slave is
+	--signal temp : std_logic_vector(7 downto 0);
 	signal temp_Data:  unsigned(7 downto 0);
 	signal WrCount  : integer range 0 to 8;
 	signal piso_reg_Data: std_logic_vector(7 downto 0);
 	signal a : std_logic;
+	type state is (idle, addr_get, ACK_Slave, Sub_Reg, Ack_Reg, Wr, Rd, Ack_Slave2, Ack_Master );
+	signal curr_state : state;
+	signal sda_trig:std_logic := '0';
+	signal SDA_slave:std_logic := '0';
 begin
-	SDA <= temp_data(7);
-	temp <="10101010";
-a <= and temp;
-Data_PISO:	process(clk,rst)
+--	SDA <= temp_data(7);
+	--temp <="10101010";
+--a <= and temp;
+SLAVE_FSM:	process(sclk,rst)
 			begin
 			if(rst = '1')then
 				piso_reg_Data <= Data_Txn;
 				temp_Data <= unsigned(piso_reg_Data);				
-			elsif(falling_edge(clk))then
-
+				WrCount <= 0;
+			elsif(falling_edge(sclk))then
+				case(curr_state)is
+				when idle => if(SDA = '1')then
+								curr_state <= addr_get;
+							else 
+								curr_state <= idle;	
+							end if;	
+							sda_trig <= '0';
+				when addr_get =>						 
 					if(WrCount = 7)then
 						WrCount <= 0;
+						curr_state <= ACK_Slave;
 					else
-						temp_Data <= (shift_right((temp_data(7 downto 0)),1));			
+						temp_Data <= (SDA & (temp_Data(7 downto 1)));
 						WrCount <= WrCount +1;
 					end if ;
+				when ACK_Slave =>
+						SDA_slave <= '0';
+						sda_trig <= '1';
+						curr_state <= idle;
+				when others =>
+						curr_state <= idle;
+				end case;
 			end if;
-			
-			end process Data_PISO;
-
+			end process SLAVE_FSM;
+	SDA <= SDA_slave when sda_trig = '1' else 'Z';
 
 end test;
